ece593228119b3aa7102d40947c0bb3fecbf3c3e
you <you@examp>
2023-05-23 22:27:44 +0530

    pointer_change

--- go_labs/pointer.go
+++ go_labs/pointer.go
@@ -17,6 +17,10 @@ func one(yptr *int) {
 
 // "new" takes a type as an argument, allocates enough memory to fit a value of that type and returns a pointer to it.
 
+type Vertex struct {
+    X, Y int
+}
+
 func main() {
 	x := 5
 	zero(x) // Here x is value of zero after the function call also.
@@ -27,5 +31,11 @@ func main() {
 	yptr := new(int) // Here we were creating a memory address to store a value of integer type
 	one(yptr)
 	fmt.Println(*yptr) // garbage collected programming language which means memory is cleaned up automatically when nothing refers to it anymore.
-
+    
+    p := Vertex{1,2}
+    q := &p
+    r := &Vertex{1,2}  // q,r are pointers to a vertex
+    
+    var s *Vertex = new(Vertex)  // create ptr to new struct instance
+    fmt.Println(p,q,r,s)
 }
