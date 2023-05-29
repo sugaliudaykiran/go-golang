f21c2bd27d79e60c75b7a81e89f25833e82d6bfa
you <you@examp>
2023-05-29 20:55:17 +0530

    json push

--- go_labs/json_formatting.go
+++ go_labs/json_formatting.go
@@ -9,6 +9,20 @@ type Bird struct{
     Description string
 }
 
+type Dimension struct{
+    Height int
+    Weight int
+}
+
+// the structure of the object in our Go code. To add a nested dimensions object, lets create a dimensions struct :
+
+type Birds struct{
+    Species string
+    Description string
+    Dimensions Dimension
+}
+
+
 func main(){
     
     // structured data --> refers to data where you know the format beforehand.
@@ -41,4 +55,35 @@ func main(){
         // fmt.Printf("Birds : %+v", birdList)
         // fmt.Printf("Birds: %v, description: %v", birdList[0].Species, birdList[0].Description)    
     }
+    
+    // Nested Objects := Now, consider the case when you have a property called Dimensions, that measures the "Height and Length" of the bird in question:
+    birdJson3 := `{
+      "species": "pigeon",
+      "decription": "likes to perch on rocks",
+      "dimensions": {
+         "height": 24,
+          "width": 10
+        }
+    }`
+    
+    var birdNested Birds
+    errNest := json.Unmarshal([]byte(birdJson3), &birdNested)
+    if errNest==nil{
+        fmt.Printf("BIRD: %+v",birdNested)
+    }
+    
+    // We can unmarshal these values to their corresponding data type in Go by using primitive types:
+    numberJson := "3"
+    floatJson := "3.1412"
+    stringJson := `"bird"`
+    
+    var x int
+    var y float64
+    var z string
+    
+    json.Unmarshal([]byte(numberJson), &x)
+    json.Unmarshal([]byte(floatJson), &y)
+    json.Unmarshal([]byte(stringJson), &z)
+    
+    fmt.Printf(" \n\n %v: %T \n %v: %T \n %v: %T",x,x,y,y,z,z)
 }
